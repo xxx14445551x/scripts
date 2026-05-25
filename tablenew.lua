1d798a280f48dbbf5f4accdf761f9852ffc465cb
xxx14445551x <dimastas811@gmail.com>
2026-05-25 16:34:29 +0800

    Create tablenew.lua

---
+++ tablenew.lua
@@ -0,0 +1,5 @@
+local Scripts = {
+    {name = "Rost", img = "rbxassetid://123584685652531", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/rost.lua"},
+    {name = "KAT", img = "rbxassetid://95851706656471", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/kat.lua"}
+}
+return Scripts
