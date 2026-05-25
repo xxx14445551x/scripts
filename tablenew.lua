ec79073bbf6e76978f379e34d8a6c385cf0c655d
xxx14445551x <dimastas811@gmail.com>
2026-05-25 18:53:16 +0800

    Update tablenew.lua

--- tablenew.lua
+++ tablenew.lua
@@ -1,5 +1,6 @@
 local Scripts = {
     {name = "Rost", img = "rbxassetid://123584685652531", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/rost.lua"},
-    {name = "KAT", img = "rbxassetid://95851706656471", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/kat.lua"}
+    {name = "KAT", img = "rbxassetid://95851706656471", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/kat.lua"},
+    {name = "KnifeAbilityTest", img = "", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/katx.lua"}
 }
 return Scripts
