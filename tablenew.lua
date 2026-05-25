c4a19e78585d1b81927683065f2c2a859f977efa
xxx14445551x <dimastas811@gmail.com>
2026-05-26 00:04:14 +0800

    Update tablenew.lua

--- tablenew.lua
+++ tablenew.lua
@@ -1,6 +1,28 @@
-local Scripts = {
+local fnc = {
+    {name = "hookmetamethod", present = type(hookmetamethod) == "function"},
+    {name = "getnamecallmethod", present = type(getnamecallmethod) == "function"},
+    {name = "checkcaller", present = type(checkcaller) == "function"},
+    {name = "Drawing", present = type(Drawing) == "table" or type(Drawing) == "userdata"},
+}
+
+local suported = true
+
+for q, exe in ipairs(fnc) do
+    if not exe.present then
+        suported = false
+        break
+    end
+end
+
+if suported == false then
+	game:GetService("Players").LocalPlayer:Kick("Executor not supported")
+else
+	print("fine")
+end
+
+local scripts = {
     {name = "Rost", img = "rbxassetid://123584685652531", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/rost.lua"},
     {name = "KAT", img = "rbxassetid://95851706656471", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/kat.lua"},
     {name = "KnifeAbilityTest", img = "", load = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/katx.lua"}
 }
-return Scripts
+return scripts
